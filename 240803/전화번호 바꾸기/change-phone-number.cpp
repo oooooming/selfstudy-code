45b5969c11c4824dcc12f390c63d025f52462e5b
oooooming <144993385+oooooming@users.noreply.github.com>
2024-08-03 08:09:12 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int x, y;
+    char d;
+ 
+    cin.get();
+    cin.get();
+    cin.get();
+    cin >> d >> x >> d >> y;
+    cout << "010" << d << y << d << x;
+
+    return 0;
+}
