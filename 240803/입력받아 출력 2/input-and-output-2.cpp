9780c4403bbabc597448ac0715330a6a9a079380
oooooming <144993385+oooooming@users.noreply.github.com>
2024-08-03 08:02:30 +0900

    Title: 입력받아 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    char d;
+
+    cin >> a >> d >> b;
+    cout << a << b; 
+    
+    return 0;
+}
