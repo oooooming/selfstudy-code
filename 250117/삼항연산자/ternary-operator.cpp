569283a799a7ba308a8ad66f3858a384f9bd8922
oooooming <144993385+oooooming@users.noreply.github.com>
2025-01-17 19:57:06 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250117/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int s;
+    cin >> s;
+
+    string result = (s=100) ? "pass" : "failure";
+    cout << result;
+    
+    return 0;
+}
