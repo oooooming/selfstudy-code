a4960afdc3aae35ef25b5a01504cf5987826feb9
oooooming <144993385+oooooming@users.noreply.github.com>
2025-01-17 19:59:36 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+
+    char c = (a == 1) ? 't' : 'f';
+    cout << c;
+    
+    return 0;
+}
