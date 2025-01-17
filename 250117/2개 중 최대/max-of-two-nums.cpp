4a695f8ed17ddf776edbfcb54582eac1481e3d59
oooooming <144993385+oooooming@users.noreply.github.com>
2025-01-17 19:54:03 +0900

    Title: 2개 중 최대, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250117/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, max;
+    cin >> a >> b;
+
+    max = a > b ? a : b;
+    cout << max;
+    
+    return 0;
+}
