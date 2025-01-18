76d4822fe5b1f0a536f3c278b707250c88eb11a6
oooooming <144993385+oooooming@users.noreply.github.com>
2025-01-18 17:02:20 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250118/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    if (n<1000) cout << "no";
+    else if (n<3000) cout << "mask";
+    else cout << "book";
+    return 0;
+}
