c8ab4ba516a124cb2b6f6d471ed7ad93e3d0956d
oooooming <144993385+oooooming@users.noreply.github.com>
2025-01-18 17:09:22 +0900

    Title: 알파벳에 따른 평가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250118/알파벳에 따른 평가/evaluation-by-alphabet.cpp
+++ 250118/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -7,11 +7,11 @@ int main() {
     cin >> c;
 
     if (c == 'S') cout << "Superior";
-    else if (c == 'A') cout << "Exellent";
+    else if (c == 'A') cout << "Excellent";
     else if (c == 'B') cout << "Good";
     else if (c == 'C') cout << "Usually";
     else if (c == 'D') cout << "Effort";
     else cout << "Failure";
-    
+
     return 0;
 }
