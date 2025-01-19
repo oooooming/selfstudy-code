3206919b7576a57c5a1bc8b7f688de5a202fe432
oooooming <144993385+oooooming@users.noreply.github.com>
2025-01-19 21:18:52 +0900

    Title: 시력 검사, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250119/시력 검사/eye-test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a;
+    cin >> b;
+
+    if (a<0.5 && b<0.5) cout << "Low";
+    else if (a<1.0 && b<1.9) cout << "Middle";
+    else cout << "High";
+    
+    return 0;
+}
