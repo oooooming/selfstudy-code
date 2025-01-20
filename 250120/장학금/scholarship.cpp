1b73eadae6cdb0c32e9b7f971ede68f26bc38cfa
oooooming <144993385+oooooming@users.noreply.github.com>
2025-01-20 19:14:30 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250120/장학금/scholarship.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int m, f;
+    cin >> m >> f;
+
+    if (m>=90) {
+        if (f>=85) cout << 100000;
+        else if (f>=90) cout << 50000;
+        else cout << 0;
+    }
+    else cout << 0;
+    return 0;
+}
