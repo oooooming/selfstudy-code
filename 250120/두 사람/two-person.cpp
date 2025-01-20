45add0ea63b04dbd96e88a9cd35ffe44790758cf
oooooming <144993385+oooooming@users.noreply.github.com>
2025-01-20 19:54:45 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250120/두 사람/two-person.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int age1, age2;
+    char sex1, sex2;
+    cin >> age1 >> sex1;
+    cin >> age2 >> sex2;
+
+    if (((age1>=19)&&(sex1=='M'))||((age2>=19)&&(sex2=='M'))) cout << 1;
+    else cout << 0;
+
+    return 0;
+}
