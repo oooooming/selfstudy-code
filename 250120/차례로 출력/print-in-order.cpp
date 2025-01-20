6e94b29371360a0bdad4587af14f9234d7a31e6d
oooooming <144993385+oooooming@users.noreply.github.com>
2025-01-20 21:01:48 +0900

    Title: 차례로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250120/차례로 출력/print-in-order.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    for (int a=5; a<18; a++) {
+        cout << a << " ";
+    }
+    
+    return 0;
+}
