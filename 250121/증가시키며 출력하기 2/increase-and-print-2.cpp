d159ccd4fc5c7afc165ac9fcda2a45195366f768
oooooming <144993385+oooooming@users.noreply.github.com>
2025-01-21 15:45:32 +0900

    Title: 증가시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250121/증가시키며 출력하기 2/increase-and-print-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a=10;
+    while (a<=26) {
+        cout << a << " ";
+        a++;
+    }
+    return 0;
+}
