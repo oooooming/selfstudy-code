0cade792b4044d4aaff6789a5823c6824e114eb2
oooooming <144993385+oooooming@users.noreply.github.com>
2025-01-21 15:40:39 +0900

    Title: 특정 숫자 도달하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250121/특정 숫자 도달하기 2/reaching-specific-number-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for (n; n>=1; n--) cout << n << " ";
+    
+    return 0;
+}
